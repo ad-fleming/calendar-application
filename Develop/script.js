a5f0ceea7ac8219bf3c213a53b1d465cebf5dc72
ad-fleming <adfleming@gmail.com>
2020-09-10 21:19:31 -0400

    updated currentDateEl to have a text that is linked to momentjs

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,9 @@
 console.log("hello world");
 
-// Define essential variables
-// =======================
+
+
+// =====================Define global variables=========================
+// =====================================================================
 var timesArray = [
     "9 AM", 
     "10 AM",
@@ -17,30 +19,51 @@ var timesArray = [
 var currentDayEl = $("#currentDay");
 var calendarContainerEl = $(".calendarContainer");
 
+// ========================Set currentDayEl using momentjs=============
+// ====================================================================
+var currentDate = moment().format('dddd, MMMM Do');
+currentDayEl.text(currentDate);
+
+
+
+
 // =============== Dynamically render time blocks =====================
+// ====================================================================
 // for an amount of times equal to timesArray.length -1
-
-// 1)create a row el and assign it to the variable of row
-    // 1.1) set attribute on that row to give it a class="time-block"
-
+for (var i =0; i < timesArray.length; i++){
+// 1)create a row el and assign it to the variable of calRow
+    var calRow = $("<row>");
+    // 1.1) set attribute on that row to give it a class=" row time-block
+    calRow.attr("class", "row  time-block");
 // 2)create a div el and assign it to a variable of hourLabel
+    var hourLabel = $("<div>");
     // 2.1) set attribute on that div to give it a class = "col-sm-1 hour"
+    hourLabel.attr("class", "col-sm-1 hour");
     // 2.2) set the text value of that div to equal to timesArray[i]
-
+    hourLabel.text(timesArray[i]);
 // 3) create a text area element and assign it to a variable of textArea
+    var textArea = $("<textarea>");
     // 3.1) set attribute on that text area to give it a class = "col-sm-10 description"
+    textArea.attr("class", "col-sm-10 description");
     // 3.2) ==optional=== set an attribute to give that text area a placeholder text of "enter calendar item here"
-
+    textArea.attr("placeholder", "What's on the schedule?");
 // 4) create a button element and assign it to a variable of saveButton
+    var saveButton = $("<button>");
     // 4.1)set attributes on the button to give it a class="col-sm-1 saveBtn" TODO:Possibly add additional class for Font Awesome Icon
+    saveButton.attr("class", "col-sm-1 saveBtn");
     // 4.2)===Temporary until Font Awesome class is added=== set the text value of the button to be "Save"
-
-// 5) Append the row to the calendarContainerEl(defined in global)
-// 6) Append the hourLabel to the row
-// 7) Append the textArea to the row
-// 8) Append the saveButton to the row
-
+    saveButton.text("Book it!");
+// 5) Append the hourLabel to the row
+    calRow.append(hourLabel);
+// 6) Append the textArea to the row
+    calRow.append(textArea);
+// 7) Append the saveButton to the row
+    calRow.append(saveButton);
+// 8) Append the row to the calendarContainerEl(defined in global)
+    calendarContainerEl.append(calRow);
+}
 // ============================ Loop End ===============================
+// ====================================================================
     
 
 
